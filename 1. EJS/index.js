512f772d5e7acf15057c4b312d667759aedbc58c
devesh1808 <devesh1808@gmail.com>
2024-12-12 10:05:47 +0530

    printed message using get method and ejs

--- 1. EJS/index.js
+++ 1. EJS/index.js
@@ -3,10 +3,21 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const d = new Date();
-  let day = d.getDay();
-  if (day == 0 || day == 6) res.send("It's a weekend, let's have some fun");
-  res.send("It's a weekday, let's work hard");
+  const today = new Date();
+  let day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day == 0 || day == 6) {
+    type = "a weekend";
+    adv = "it's time to have fun";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
 });
 
 app.listen(port, () => {
